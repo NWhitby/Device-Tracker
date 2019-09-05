811c301828103213383c94646ceddac7fb2923af
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-05 14:30:58 -0400

    Add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
 
   belongs_to :commentor, :class_name => "User", :foreign_key => "user_id"
   belongs_to :device  
+  
+  validates :content, presence: true, length: { maximum: 1000 }
 
 end
