ea8795818dab247ce44ffdf59e3765138d695a57
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-05 14:13:52 -0400

    Generate sessions controller and add sign up, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout' 
   resources :comments
   resources :devices
   resources :users
