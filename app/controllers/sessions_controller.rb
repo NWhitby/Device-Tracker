4888994b7997f720c4b594ec168ff51a9b395491
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-05 14:21:34 -0400

    Add login and logout functionality/sessions new tememplate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
-class SessionsController < ApplicationController 
+class SessionsController < ApplicationController
+   
+  def new
+    @user = User.new
+  end
+
+   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:username])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+   def destroy
+    session.destroy
+    redirect_to root_path
+  end
 
 end
