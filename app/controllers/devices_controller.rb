e5773f02a724591c9a315e5cb6df353e0d84cb5e
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 15:03:19 -0400

    Add new and create actions

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,10 +2,26 @@ class DevicesController < ApplicationController
 
   def index
     @user = User.find_by(id: params[:user_id])
+    
     if @user
       @devices = @user.devices.all
     else
       @devices = Device.all
+    end
+  end
+
+  def new
+    @device = Device.new
+  end
+
+  def create
+    @user = current_user
+
+    @device = current_user.devices.build(device_params)
+    if @device.save
+      redirect_to user_devices_path(current_user)
+    else
+      render 'new'
     end
   end
   
