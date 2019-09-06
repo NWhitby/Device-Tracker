f7a302defa0c2d490586ca2a8e8f70b668852095
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 15:10:58 -0400

    Add create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  before_action :require_login
+
+  def create
+    @user = current_user
+    @device = Device.find(params[:device_id])
+    @comment = @device.comments.create(comment_params)
+
+    if @comment.save
+      redirect_to device_path(@device)
+    else
+      redirect_to device_path(@device)
+    end
+  end
+
 end
--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,4 +1,5 @@
 class DevicesController < ApplicationController
+  
   before_action :require_login, except: [:index]
 
   def index
