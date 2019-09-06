1d1431595f0cf4b917f46d93e7fa76d6cd5dea2a
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 14:19:56 -0400

    Add logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  
+
   private 
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
     end
+
+    def logged_in?
+      !!current_user
+    end
+  
 
 end
