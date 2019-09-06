d33ccf369480e2cbdd3580e82f9e7fa0500a7255
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 14:41:12 -0400

    Nest devices and comments routes

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,2 +1,12 @@
 class DevicesController < ApplicationController
+
+  def index
+    @user = User.find_by(id: params[:user_id])
+    if @user
+      @devices = @user.devices.all
+    else
+      @devices = Device.all
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy', as: 'logout' 
-  resources :comments
-  resources :devices
-  resources :users
+  get '/logout', to: 'sessions#destroy', as: 'logout'   
+  
+  resources :users, only: [:new, :create, :show] do
+    resources :devices, only: [:index, :new, :create, :show, :update]
+  end
+  
+  resources :devices, only: [:show, :edit, :update, :destroy] do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
