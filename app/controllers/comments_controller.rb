26e44217926aa689016d3e7271865f59670e6a0f
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 15:26:18 -0400

    Add facebook auth and recent route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,8 +28,8 @@ class CommentsController < ApplicationController
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:user_id, :device_id, :content)
-  end
+    def comment_params
+      params.require(:comment).permit(:user_id, :device_id, :content)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy', as: 'logout'   
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/devices/recent', to: 'devices#recent'   
   
   resources :users, only: [:new, :create, :show] do
     resources :devices, only: [:index, :new, :create, :show, :update]
