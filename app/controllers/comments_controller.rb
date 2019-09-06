09a8323a47137b5526603051f78a16932476dea3
NWhitby <35510733+NWhitby@users.noreply.github.com>
2019-09-06 15:19:07 -0400

    Add comment params and destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,4 +14,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @device= Device.find(params[:hdevice_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user.id == @comment.user_id
+      @comment.destroy
+      redirect_to device_path(@device)
+    else
+      redirect_to device_path(@device)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :device_id, :content)
+  end
+
 end
